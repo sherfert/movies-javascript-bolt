c9cc93d7361188bf838df300e68a8503a4f0f7a5
Satia Herfert <satiaherfert@gmx.de>
2018-05-03 16:25:16 +0200

    Hackathon: Space and DateTime search

--- src/models/Accident.js
+++ src/models/Accident.js
@@ -1,14 +1,11 @@
 var _ = require('lodash');
 
-function Movie(_node) {
+function Accident(_node) {
   _.extend(this, _node.properties);
 
   if (this.id) {
     this.id = this.id.toNumber();
   }
-  if (this.duration) {
-    this.duration = this.duration.toNumber();
-  }
 }
 
-module.exports = Movie;
+module.exports = Accident;
--- src/neo4jApi.js
+++ src/neo4jApi.js
@@ -1,50 +1,25 @@
 require('file?name=[name].[ext]!../node_modules/neo4j-driver/lib/browser/neo4j-web.min.js');
-var Movie = require('./models/Movie');
-var MovieCast = require('./models/MovieCast');
+var Accident = require('./models/Accident');
 var _ = require('lodash');
 
 var neo4j = window.neo4j.v1;
-var driver = neo4j.driver("bolt://localhost", neo4j.auth.basic("neo4j", "abcde"));
+var driver = neo4j.driver("bolt://localhost", neo4j.auth.basic("neo4j", "neo5j"));
 
-function searchMovies(queryString) {
+function getAccident(number) {
   var session = driver.session();
   return session
     .run(
-      'MATCH (movie:Movie) \
-      WHERE movie.title =~ {title} \
-      RETURN movie',
-      {title: '(?i).*' + queryString + '.*'}
-    )
-    .then(result => {
-      session.close();
-      return result.records.map(record => {
-        return new Movie(record.get('movie'));
-      });
-    })
-    .catch(error => {
-      session.close();
-      throw error;
-    });
-}
-
-function getMovie(title) {
-  var session = driver.session();
-  return session
-    .run(
-      "MATCH (movie:Movie {title:{title}}) \
-      OPTIONAL MATCH (movie)<-[r]-(person:Person) \
-      RETURN movie.title AS title, \
-      collect([person.name, \
-           head(split(lower(type(r)), '_')), r.roles]) AS cast \
-      LIMIT 1", {title})
+      "MATCH (acc:Accident {number:{number}}) \
+      RETURN acc\
+      LIMIT 1", {number})
     .then(result => {
       session.close();
 
       if (_.isEmpty(result.records))
         return null;
 
-      var record = result.records[0];
-      return new MovieCast(record.get('title'), record.get('cast'));
+      var record = result.records[0].get('acc');
+      return new Accident(record);
     })
     .catch(error => {
       session.close();
@@ -52,37 +27,74 @@ function getMovie(title) {
     });
 }
 
-function getGraph() {
-  var session = driver.session();
-  return session.run(
-    'MATCH (m:Movie)<-[:ACTED_IN]-(a:Person) \
-    RETURN m.title AS movie, collect(a.name) AS cast \
-    LIMIT {limit}', {limit: 100})
-    .then(results => {
-      session.close();
-      var nodes = [], rels = [], i = 0;
-      results.records.forEach(res => {
-        nodes.push({title: res.get('movie'), label: 'movie'});
-        var target = i;
-        i++;
-
-        res.get('cast').forEach(name => {
-          var actor = {title: name, label: 'actor'};
-          var source = _.findIndex(nodes, actor);
-          if (source == -1) {
-            nodes.push(actor);
-            source = i;
-            i++;
-          }
-          rels.push({source, target})
+function searchAccidents(queryString) {
+    var session = driver.session();
+    return session
+        .run(
+            'MATCH (acc:Accident)\
+             WHERE acc.number STARTS WITH {queryString}\
+             RETURN acc\
+             LIMIT 10', {queryString})
+        .then(result => {
+            session.close();
+            return result.records.map(r => {
+                var record = r.get('acc');
+                return new Accident(record);
+            });
         })
-      });
-
-      return {nodes, links: rels};
-    });
+        .catch(error => {
+            session.close();
+            throw error;
+        });
 }
 
-exports.searchMovies = searchMovies;
-exports.getMovie = getMovie;
-exports.getGraph = getGraph;
+function searchAccidentsOnDayAndLocation(year, month, day, lat, lng, rad) {
+    var session = driver.session();
+    console.log(rad);
+    return session
+        .run(
+            'WITH date({ year: toInt({year}), month: toInt({month}) + 1, day: toInt({day}) }) as d\
+             WITH localdatetime({date: d, time: time("00:00") }) as start \
+             WITH start, start + duration("P1D") as end\
+             MATCH (acc:Accident)\
+             WHERE start < acc.dt < end AND distance(acc.p, point({latitude: {lat}, longitude: {lng} })) < toInt({rad}) \
+             RETURN acc', {year: year, month: month, day: day, lat:lat, lng: lng, rad: rad})
+        .then(result => {
+            session.close();
+            return result.records.map(r => {
+                var record = r.get('acc');
+                return new Accident(record);
+            });
+        })
+        .catch(error => {
+            session.close();
+            throw error;
+        });
+}
+
+function streamAccidents() {
+    var session = driver.session();
+    return session
+        .run(
+            'MATCH (acc:Accident)\
+             RETURN acc\
+             ORDER BY acc.dt\
+             LIMIT 100')
+        .then(result => {
+            session.close();
+            return result.records.map(r => {
+                var record = r.get('acc');
+                return new Accident(record);
+            });
+        })
+        .catch(error => {
+            session.close();
+            throw error;
+        });
+}
+
+exports.getAccident = getAccident;
+exports.searchAccidents = searchAccidents;
+exports.searchAccidentsOnDayAndLocation = searchAccidentsOnDayAndLocation;
+exports.streamAccidents = streamAccidents;
 
